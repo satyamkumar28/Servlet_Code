f9bc6274a8883a30c7d25f284f5f7c1f9b0ed19a
satyamkumar28 <satyam.j2ee2016@gmail.com>
2016-12-09 20:15:58 +0530

    Fourth class of Servlet

---
+++ app4/WEB-INF/src/com/lara/Servlet4.java
@@ -0,0 +1,23 @@
+package  com.lara;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Servlet4 extends HttpServlet{
+
+    public void service(HttpServletRequest r1,HttpServletResponse r2)throws ServletException ,IOException{
+
+        String s1 =r1.getParameter("num1");
+        String s2 = r1.getParameter("num2");
+
+        int num1=Integer.parseInt(s1);
+        int num2=Integer.parseInt(s2);
+        PrintWriter out = r2.getWriter();
+
+        out.println("Number1:"+num1);
+        out.println("Number2:"+num2);
+
+        out.println("Compare:"+(num2==num1));
+    }
+}
